7b22940127deee28bea827678f47d055430545cc
aominehayato <aominehayato@outlook.jp>
2026-07-23 11:27:36 +0900

    Update playlist.js

--- routes/playlist.js
+++ routes/playlist.js
@@ -1,13 +1,14 @@
 import express from "express";
-import { getYouTube } from "../utils/youtube.js";
+import { getYouTube, resetYouTubeIfCritical } from "../utils/youtube.js";
+import { videoLimiter } from "../utils/limiter.js";
+import logger from "../utils/logger.js";
 
 const router = express.Router();
 
 /**
  * GET /api/playlist/:id
- * プレイリスト内の動画一覧を取得する
  */
-router.get("/:id", async (req, res) => {
+router.get("/:id", videoLimiter, async (req, res) => {
   const playlistId = req.params.id;
 
   try {
@@ -19,8 +20,9 @@ router.get("/:id", async (req, res) => {
 
     for (let i = 0; i < contents.length; i++) {
       const item = contents[i];
+      if (!item) continue;
       items.push({
-        id: item.id,
+        id: item.id || "",
         title: item.title?.text || item.title || "",
         thumbnail: item.thumbnails?.[0]?.url || "",
         author: item.author?.name || ""
@@ -33,7 +35,8 @@ router.get("/:id", async (req, res) => {
     });
 
   } catch (error) {
-    console.error("Playlist error for ID " + playlistId + ":", error);
+    logger.error({ err: error, playlistId }, "Playlist error");
+    resetYouTubeIfCritical(error);
     res.status(500).json({ error: error.message });
   }
 });
